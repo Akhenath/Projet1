05ed1acb6fbc039d22047453dac68ce06b1d2d36
florian.greck <florian.greck@CPX-C35MNN76DFK.dir.svc.accenture.com>
2018-10-15 16:34:29 +0200

    Fix URL

--- Pokemon/javascript/cours.js
+++ Pokemon/javascript/cours.js
@@ -39,14 +39,3 @@ function ContentFrame(page) {
 	document.getElementById("contentframe").src = page;
 
 }
-
-function menuDeroulant(id)
-{
-    var element = document.getElementById(id);
-    var etat = element.style.display; 
-     
-    if (etat == none)
-        element.style.display = 'block';
-    else
-        element.style.display = 'none';
-}
