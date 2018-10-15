c1bcfd637e7a7a105fada756c815dcededad269b
florian.greck <florian.greck@CPX-C35MNN76DFK.dir.svc.accenture.com>
2018-10-15 15:39:17 +0200

    CSS

---
+++ Pokemon/javascript/cours.js
@@ -0,0 +1,52 @@
+//Onglet Refresh
+function refresh() {
+	document.getElementById("contentframe").style.visibility = "hidden";
+	document.getElementById("gen1_1").style.visibility = "hidden";
+	document.getElementById("gen2_1").style.visibility = "hidden";
+	document.getElementById("gen3_1").style.visibility = "hidden";
+
+}
+
+//Onglet Génération 1
+function gen1() {
+	document.getElementById("contentframe").style.visibility = "hidden";
+	document.getElementById("gen1_1").style.visibility = "visible";
+	document.getElementById("gen2_1").style.visibility = "hidden";
+	document.getElementById("gen3_1").style.visibility = "hidden";
+
+}
+
+//Onglet Génération 2
+function gen2() {
+	document.getElementById("contentframe").style.visibility = "hidden"; 
+	document.getElementById("gen1_1").style.visibility = "hidden";
+	document.getElementById("gen2_1").style.visibility = "visible";
+	document.getElementById("gen3_1").style.visibility = "hidden";
+}
+
+//Onglet Génération 3
+function gen3() {
+	document.getElementById("contentframe").style.visibility = "hidden";
+	document.getElementById("gen1_1").style.visibility = "hidden";
+	document.getElementById("gen2_1").style.visibility = "hidden";
+	document.getElementById("gen3_1").style.visibility = "visible";
+
+}
+
+//Frame HTML implémentée
+function ContentFrame(page) {
+	document.getElementById("contentframe").style.visibility = "visible";
+	document.getElementById("contentframe").src = page;
+
+}
+
+function menuDeroulant(id)
+{
+    var element = document.getElementById(id);
+    var etat = element.style.display;
+     
+    if (etat == none)
+        element.style.display = 'block';
+    else
+        element.style.display = 'none';
+}
